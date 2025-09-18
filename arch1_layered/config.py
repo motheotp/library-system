50abbcd4113437ad56781f7ed0674d7b73a35777
aashish177 <aashishmah177@gmail.com>
2025-09-18 11:34:49 -0500

    added routes and Dockerfile

--- arch1_layered/config.py
+++ arch1_layered/config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    DATABASE_URL = os.getenv()
+    DATABASE_URL = os.getenv('DATABASE_URL')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
     REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
